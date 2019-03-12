ce4fc201d2fa97068d58d057327f4d723a1dfc42
TarasPyzo <taraspyzo@gmail.com>
2019-03-12 19:56:06 +0200

    commit test 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+![GitHub top language](https://img.shields.io/github/languages/top/TarasPyzo/node-test-dump.svg)
 readme
--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var assert = require('chai').assert;
 suite('Array', function() {
   suite('#indexOf()', function() {
     test('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), 1);
+      assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
 });
